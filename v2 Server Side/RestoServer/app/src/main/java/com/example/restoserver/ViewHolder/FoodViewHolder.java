72da7c3f9d376313e6ec19ee1968a0ab513bfcdf
Dadda Ziyad <59479326+InfernoZed@users.noreply.github.com>
2020-05-31 19:36:17 +0100

    v2.0 final

--- v2 Server Side/RestoServer/app/src/main/java/com/example/restoserver/ViewHolder/FoodViewHolder.java
+++ v2 Server Side/RestoServer/app/src/main/java/com/example/restoserver/ViewHolder/FoodViewHolder.java
@@ -1,15 +1,19 @@
 package com.example.restoserver.ViewHolder;
 
+import android.view.ContextMenu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.restoserver.Common.Common;
 import com.example.restoserver.Interface.ItemClickListener;
 import com.example.restoserver.R;
 
-public class FoodViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+public class FoodViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,
+        View.OnCreateContextMenuListener
+{
 
     public TextView food_name;
     public ImageView food_image;
@@ -22,6 +26,7 @@ public class FoodViewHolder extends RecyclerView.ViewHolder implements View.OnCl
         food_name = itemView.findViewById(R.id.food_name);
         food_image = itemView.findViewById(R.id.food_image);
 
+        itemView.setOnCreateContextMenuListener(this);
         itemView.setOnClickListener(this);
 
     }
@@ -34,5 +39,13 @@ public class FoodViewHolder extends RecyclerView.ViewHolder implements View.OnCl
     public void onClick(View view)
     {
         itemClickListener.onClick(view, getAdapterPosition(), false);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+        contextMenu.setHeaderTitle("Delete food ");
+
+        contextMenu.add(0,0,getAdapterPosition(), Common.UPDATE);
+        contextMenu.add(0,1,getAdapterPosition(), Common.DELETE);
     }
 }
